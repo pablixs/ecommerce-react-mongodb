069b097df4d66a046e802f817ff92551effcbda8
unknown <pablicjs@gmail.com>
2023-01-18 00:26:05 -0300

    Routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, useParams } from "react-router-dom";
 import { Container } from 'react-bootstrap'; 
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -14,7 +14,7 @@ const App = () => {
         <Container>
           <Routes>
             <Route path='/' element={<HomeScreen/>} exact />
-            <Route path='/product/:id' element={<ProductScreen/>} />
+            <Route path='/product/:id'  element={<ProductScreen/>} />
           </Routes>
         </Container>
       </main>
